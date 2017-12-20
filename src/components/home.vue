85d63ae24c4cb4b835376e7817b7034b6ba7dd8b
liupeng <1129638103@qq.com>
2017-12-20 16:45:18 +0800

    随便弄着玩，要做工作项目了

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,53 +1,50 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="home">
+        <div class="background">
+            <section>
+                <h3>我有一个呆宝宝</h3>
+                <h3>她是一个大哈宝</h3>
+            </section>
+            <img src="../../static/dai1.jpg" alt="呆宝宝">
+            <section>
+                <h3>她为什么这么哈</h3>
+                <h3>我也不晓得的嘛</h3>
+            </section>
+            <img src="../../static/dai2.jpg" alt="我爱你">
+            <section>
+                <h3>爱你哟，呆宝宝</h3>
+            </section>
+            <img src="../../static/dai4.jpg" alt="我爱你">
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'hello',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+    export default {
+        name: 'hello',
+        data() {
+            return {
+
+            }
+        }
     }
-  }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+<style scoped lang="less">
+    .home{
+        background-color: rgba(248, 251, 117, 0.1);
+    }
+    section{
+        text-align: center;
+    }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+    .background{
+        img{
+            width: 21rem;
+            display: block;
+            height: 14rem;
+            margin: 20px auto;
+        }
+    }
 </style>
