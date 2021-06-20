528c5879c0268004865a91570e8d58ac3c4b535e
spiritsliu <spiritsliu@tencent.com>
2021-06-20 23:16:35 +0800

    feat: 修改配置

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@ git pull origin master;
 docker stop pw_front;
 docker rm pw_front;
 docker build -t pw_front .;
-docker run --network=pw_network -p 80:52199 -d -v /etc/localtime:/etc/localtime --name pw_front pw_front:latest;
+docker run --network=pw_network -p 80:52199 -itd -v /etc/localtime:/etc/localtime --name pw_front pw_front:latest;
 docker image prune -f; # f不用确认
