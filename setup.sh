bb0b8a11531230b03c0528076230bf8b19d66edd
spiritsliu <spiritsliu@tencent.com>
2021-06-21 11:08:49 +0800

    feat: 部署脚本修改

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ docker rm pw_front;
 docker build -t pw_front .;
 docker run --network=pw_network -p 80:52199 -itd -v /etc/localtime:/etc/localtime --name pw_front pw_front:latest;
 docker image prune -f; # f不用确认
+docker start mongo_spirits pw_back pw_front
