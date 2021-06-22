a45edeadb98bddc2cb8fc62f41d5f12e9bf431e4
spiritsliu <spiritsliu@tencent.com>
2021-06-22 10:23:03 +0800

    feat: 修改构建脚本

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 git pull origin master;
-docker stop pw_front;
+docker stop pw_front pw_back mongo_spirits;
 docker rm pw_front;
 docker build -t pw_front .;
 docker run --network=pw_network -p 80:52199 -itd -v /etc/localtime:/etc/localtime --name pw_front pw_front:latest;
